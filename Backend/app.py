fa3e4e0e42ff0d550fed9577fdf3615928cd2b13
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-14 14:32:58 +0500

    Debugging

--- Backend/app.py
+++ Backend/app.py
@@ -1,42 +1,15 @@
-from flask import Flask, request, send_file
-import os
-from pytubefix import YouTube
-import subprocess
+# app.py
+from flask import Flask
 
 app = Flask(__name__)
 
-# def youtube_to_mp3(youtube_url, output_path="output"):
-#     try:
-#         # Create output directory if it doesn't exist
-#         os.makedirs(output_path, exist_ok=True)
+@app.route('/', methods=['GET'])
+def home():
+    return "Homepage"
 
-#         # Download YouTube video
-#         yt = YouTube(youtube_url)
-#         video = yt.streams.filter(only_audio=True).order_by('abr').last()
-#         download_path = video.download(output_path)
-
-#         # Convert to MP3 using ffmpeg
-#         base, ext = os.path.splitext(download_path)
-#         mp3_path = f"{base}.mp3"
-
-#         # Only convert if the file is not already in MP3 format
-#         if ext.lower() != ".mp3":
-#             subprocess.run(["ffmpeg", "-i", download_path, "-q:a", "0", "-map", "a", mp3_path])
-#             os.remove(download_path)  # Clean up the original file
-#         else:
-#             mp3_path = download_path
-
-#         return mp3_path
-#     except Exception as e:
-#         return str(e)
-
-@app.route('/convert', methods=['GET'])
-def convert_video():
-    return "<p>Hello World</p>"
-    # url = request.json['url']
-    # try:
-    #     mp3_file = youtube_to_mp3(url)
-    #     return send_file(mp3_file, as_attachment=True)
-    # except Exception as e:
-    #     return {'error': str(e)}, 400
-
+@app.route('/contact', methods=['GET'])
+def contact():
+    return "Contact page"
+    
+if __name__ == "__main__":
+    app.run() 
