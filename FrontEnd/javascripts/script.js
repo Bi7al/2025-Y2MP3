79368238dbda03deae3dc9720a3c0cb15c02aa5d
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-14 15:17:10 +0500

    Linking Front-End and Back-End

---
+++ FrontEnd/javascripts/script.js
@@ -0,0 +1,32 @@
+document.getElementById('convertBtn').addEventListener('click', function () {
+
+    const url = document.getElementById('url').value;
+
+    fetch('https://2025-y2-mp-3.vercel.app/convert', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.blob();
+            } else {
+                throw new Error('Conversion failed');
+            }
+        })
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'output.mp3';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
