bd77a74c69dee68eed58f70b7e21900bd4acd44f
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-14 15:10:52 +0500

    SERVER Running

--- Backend/app.py
+++ Backend/app.py
@@ -46,11 +46,12 @@ def home():
 def convert_video():
     url = request.json['url']
     if(request.method=='POST'):
-        try:
-            mp3_file = youtube_to_mp3(url)
-            return send_file(mp3_file, as_attachment=True)
-        except Exception as e:
-            return {'error': str(e)}, 400
+        return url
+        # try:
+        #     mp3_file = youtube_to_mp3(url)
+        #     return send_file(mp3_file, as_attachment=True)
+        # except Exception as e:
+        #     return {'error': str(e)}, 400
     else:
         return "Enter Correct URl"
     
